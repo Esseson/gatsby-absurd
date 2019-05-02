ed8911a245b6d86ae93c28f9c755e8557dd49bbe
Esseson <35272146+Esseson@users.noreply.github.com>
2019-05-02 05:07:59 -0700

    Updating header Navigation

--- src/components/common/Navbar/Navbar.js
+++ src/components/common/Navbar/Navbar.js
@@ -15,7 +15,7 @@ import {
 
 import { ReactComponent as MenuIcon } from '@static/icons/menu.svg';
 
-const NAV_ITEMS = ['About', 'Brands', 'Team', 'FAQ'];
+const NAV_ITEMS = ['About Yope', 'How Yope Works', 'Contact Us', 'FAQ'];
 
 class Navbar extends Component {
   state = {
