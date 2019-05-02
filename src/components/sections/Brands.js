41a1dc332c93da1b5c6902733a596900be13a0fe
Esseson <Essesonahmed@yahoo.com>
2019-05-02 07:02:51 -0700

    How it works update

--- src/components/sections/Brands.js
+++ src/components/sections/Brands.js
@@ -1,44 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { StaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
 
 import { Section, Container } from '@components/global';
-import ExternalLink from '@common/ExternalLink';
-
-import { ReactComponent as AirbnbLogo } from '@static/logos/airbnb.svg';
-import { ReactComponent as AppleMusicLogo } from '@static/logos/apple-music.svg';
-import { ReactComponent as CokeLogo } from '@static/logos/coca-cola.svg';
-import { ReactComponent as NodeLogo } from '@static/logos/nodejs.svg';
-import { ReactComponent as NikeLogo } from '@static/logos/nike.svg';
-import { ReactComponent as InstagramLogo } from '@static/logos/instagram.svg';
-
-const LOGOS = [
-  {
-    logo: AirbnbLogo,
-    link: 'https://airbnb.io',
-  },
-  {
-    logo: AppleMusicLogo,
-    link: 'https://www.apple.com/in/music/',
-  },
-  {
-    logo: CokeLogo,
-    link: 'https://coca-cola.com',
-  },
-  {
-    logo: NodeLogo,
-    link: 'https://nodejs.org',
-  },
-  {
-    logo: NikeLogo,
-    link: 'https://nike.com',
-  },
-  {
-    logo: InstagramLogo,
-    link: 'https://instagram.com',
-  },
-];
 
 const UsedBy = () => (
   <StaticQuery
@@ -60,39 +24,14 @@ const UsedBy = () => (
       <Section id="how yope works" accent>
         <StyledContainer>
           <div>
-            <h1>Used by biggest in tech</h1>
-            <LogoGrid>
-              {LOGOS.map(({ logo, link }) => (
-                <ExternalLink href={link}>{logo()}</ExternalLink>
-              ))}
-            </LogoGrid>
+            <h1>How Yope Works</h1>
           </div>
-          <Art>
-            <Img fluid={data.art_story.childImageSharp.fluid} />
-          </Art>
         </StyledContainer>
       </Section>
     )}
   />
 );
 
-const LogoGrid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 64px;
-  justify-items: center;
-  margin-top: 96px;
-
-  a {
-    svg {
-      width: 100%;
-    }
-  }
-
-  @media (max-width: ${props => props.theme.screen.sm}) {
-    grid-template-columns: 1fr;
-  }
-`;
 
 const StyledContainer = styled(Container)`
   display: flex;
@@ -101,23 +40,6 @@ const StyledContainer = styled(Container)`
 
   @media (max-width: ${props => props.theme.screen.md}) {
     justify-content: center;
-  }
-`;
-
-const Art = styled.figure`
-  width: 600px;
-  position: absolute;
-  top: -12%;
-  right: 50%;
-
-  @media (max-width: ${props => props.theme.screen.lg}) {
-    top: 0;
-    right: 65%;
-    width: 500px;
-  }
-
-  @media (max-width: ${props => props.theme.screen.md}) {
-    display: none;
   }
 `;
 
